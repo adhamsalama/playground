6970e173d277aabc4dcb5f46ec659d694105a48e
Adham Salama <adham.salama@wuilt.com>
2023-01-01 22:00:04 +0200

    Finish guessing example

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,32 +1,28 @@
 // https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-
     println!("Please input your guess.");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    let mut guess = String::new();
-    let guess_int: i32;
-    match io::stdin().read_line(&mut guess) {
-        Ok(_n) => {
-            println!("{_n} bytes read");
-            guess_int = guess.trim().parse().unwrap(); // or let guess_int = guess.trim().parse::<i32>().unwrap();
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => print!("Too small!\n"),
+            Ordering::Greater => print!("Too big!\n"),
+            Ordering::Equal => {
+                print!("You win!\n");
+                break;
+            }
         }
-        Err(error) => {
-            println!("error: {error}");
-            std::process::exit(1);
-        }
-    }
-
-    println!("You guessed: {guess_int}");
-    println!("Secret number is {secret_number}");
-    if guess_int == secret_number {
-        println!("You guessed right!");
-    } else {
-        println!("You guessed wrong!");
     }
 }
