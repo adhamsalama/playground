602b8bbadc8a48f83f6f6c9bb731add5ef0e6410
Adham Salama <adham.salama@wuilt.com>
2023-01-08 00:56:01 +0200

    Add generics & traits example

--- rust/generics_example/src/main.rs
+++ rust/generics_example/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let s = [String::from("a"), String::from("b")];
     let ss = array_to_vec(&s);
     println!("{:?}", ss);
+    let a = Pair { x: 1, y: 1 };
 }
 
 fn array_to_vec<T>(arr: &[T]) -> Vec<T>
@@ -16,4 +17,26 @@ where
         myvec.push(i.clone());
     }
     myvec
+}
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
 }
