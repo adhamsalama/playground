2a6169a1b6be6735930e3c2f1e9ca16e41d21c91
Adham Salama <adham.salama@wuilt.com>
2023-01-08 21:23:48 +0200

    Refactor minigrep using iterators

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -23,12 +23,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(s) => s,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(s) => s,
+            None => return Err("Didn't get a file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
@@ -39,25 +44,18 @@ impl Config {
     }
 }
 
-pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in content.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
-fn search_case_insensitive<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in content.lines() {
-        if line.to_lowercase().contains(&query.to_lowercase()) {
-            results.push(line);
-        }
-    }
-
-    results
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        // Have to use & here because to_lowercase() returns a new String, so contains takes ownership of it
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
