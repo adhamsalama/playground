46171ccd8216897acfcfe77be096bdd1880385ed
Adham Salama <adham.salama@wuilt.com>
2023-01-14 12:29:41 +0200

    Sort dataloader results by key before return because order is not guaranteed

--- graphql/social-network/src/dataloaders/dataloaders.ts
+++ graphql/social-network/src/dataloaders/dataloaders.ts
@@ -4,10 +4,10 @@ import { User } from "../models/user";
 
 export const userDataLoader = new DataLoader(async (keys) => {
   const users = await User.find({ _id: { $in: keys } });
-  return users;
+  return keys.map(key => users.find(user => user._id.toString() === key));
 });
 
 const postDataLoader = new DataLoader(async (keys) => {
   const posts = await Post.find({ user: { $in: keys } });
-  return posts;
+  return keys.map(key => posts.find(post => post._id.toString() === key));
 });
