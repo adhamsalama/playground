8f999a7e7b845c4173a1b1181c9a50def9a18b00
Adham Salama <adham.salama@wuilt.com>
2023-01-21 23:43:44 +0200

    Add concurrency example

---
+++ rust/concurrency/src/main.rs
@@ -0,0 +1,68 @@
+use std::rc::Rc;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+fn main() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let thread_id = thread::current().id();
+            println!("{:?} attpemting to acquire the lock", thread_id);
+            let mut num = counter.lock().unwrap();
+            println!("{:?} acquired the lock", thread_id);
+            thread::sleep(Duration::from_secs(11));
+            *num += 1;
+            println!("{:?} incremented the num", thread_id);
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
+fn channels() {
+    // --snip--
+
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+    // --snip--
+}
